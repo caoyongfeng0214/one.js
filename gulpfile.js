c8093ee3a5e0d7571d596fe35df9142c7592659a
caoyongfeng0214 <caoyongfeng0214@gmail.com>
2016-10-09 22:13:35 +0800

    added gulpfile.js package.json

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+﻿var gulp = require('gulp');
+var less = require('gulp-less');
+var uglify = require('gulp-uglify');
+var minifycss = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+
+
+gulp.task('init', function(){
+    gulp.src('source/less/one.less').pipe(less()).pipe(minifycss()).pipe(gulp.dest('source/css/'));
+    gulp.src(['source/css/ionicons.css', 'source/css/swiper.css', 'source/css/one.css']).pipe(concat('one.css')).pipe(gulp.dest('build/css/'));
+
+    gulp.src(['source/js/swiper.js', 'source/js/one.js']).pipe(concat('one.js')).pipe(uglify()).pipe(gulp.dest('build/js/'));
+
+    gulp.src('source/js/require.js').pipe(gulp.dest('build/js/'));
+    gulp.src('source/js/text.js').pipe(uglify()).pipe(gulp.dest('build/js'));
+
+    gulp.src('source/fonts/*').pipe(gulp.dest('build/fonts/'));
+});
+
+gulp.task('default', ['init']);
